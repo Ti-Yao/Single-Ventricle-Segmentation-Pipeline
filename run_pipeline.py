5b5ed39dd4fa044ce06b69049ab370318542e10d
Ti-Yao <yaotina98@gmail.com>
2026-03-10 12:06:09 +0000

    debug

--- run_pipeline.py
+++ run_pipeline.py
@@ -3,7 +3,7 @@ import pandas as pd
 from pipeline import *
 import pickle
 def main():
-    data_path = '/workspaces/Force-ML/data/dicom_data'
+    data_path = './data/debug'
     log_file = 'error_log.csv'
 
     # Try to load existing errors to append to
@@ -15,20 +15,9 @@ def main():
     patients = [pat.split('/')[-1] for pat in glob.glob(f'{data_path}/*') if 'zip' not in pat]
     print(patients)
 
-    for patient in ['BCH-BRETYL-1_2021'][:]:
+    for patient in patients[:]:
         print(patient)
-        # if not os.path.exists(f'/workspaces/Force-ML/pipeline/app/results/segs/{patient}.gif'):
-            # try:
         p = Pipeline(patient, data_path=data_path)
-            # except Exception as e:
-            #     error_entry = pd.DataFrame([{'Patient': patient, 'Error': str(e)}])  
-                
-            #     # Use pd.concat() instead of append()
-            #     df = pd.concat([df, error_entry], ignore_index=True)
-
-            #     # Save updated DataFrame to CSV immediately
-            #     df.to_csv(log_file, index=False)
-            #     print(f"Logged error for {patient}")
 
 if __name__ == "__main__":
     main()
